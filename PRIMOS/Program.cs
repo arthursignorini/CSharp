882c76196a7aec8aa2411722bdea7af9d30b6cd4
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-07-09 08:44:30 -0300

    primos

---
+++ PRIMOS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PRIMOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int cont = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    cont++;
+                }
+            }
+
+            if (cont == 2)
+            {
+                Console.WriteLine("Esse é um numero primo");
+            }
+            else
+            {
+                Console.WriteLine("Esse não é um numero primo");
+            }
+
+            Console.ReadKey(); // Manter o console aberto após a execução
+        }
+    }
+}
